5be490bc7cab1642e5b4a24e8dbeae4e4b716e57
Alex Idowu <alexidowu25@gmail.com>
2025-05-16 00:54:27 +0000

    update addons

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,15 @@ CERT_DIR="/etc/rabbitmq/certs"
 OPENSSL_CONF="$CERT_DIR/openssl-wildcard.cnf"
 SERVER_CERT="$CERT_DIR/server.crt"
 SERVER_KEY="$CERT_DIR/server.key"
+CA_CERT="$CERT_DIR/ca.crt"
 
 # Generate wildcard SSL cert if not present
 if [[ ! -f "$SERVER_CERT" || ! -f "$SERVER_KEY" ]]; then
   echo "[entrypoint] Generating wildcard SSL certificate..."
   mkdir -p "$CERT_DIR"
   openssl req -new -x509 -days 365 -nodes -out "$SERVER_CERT" -keyout "$SERVER_KEY" -config "$OPENSSL_CONF" -extensions v3_req
-  chown rabbitmq:rabbitmq "$SERVER_CERT" "$SERVER_KEY"
+  cp "$SERVER_CERT" "$CA_CERT"
+  chown rabbitmq:rabbitmq "$SERVER_CERT" "$SERVER_KEY" "$CA_CERT"
 else
   echo "[entrypoint] Using existing SSL certificates."
 fi
